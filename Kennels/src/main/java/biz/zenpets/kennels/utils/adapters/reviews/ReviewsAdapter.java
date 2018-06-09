f5284878db9bc072ce45ecc49d5f7b5fd7b86b0f
Zen Pets <info@zenpets.biz>
2018-06-09 15:01:48 +0530

    Implementing the Kennel Reviews mechanism in the Kennels module and the Pet Parents module

--- Kennels/src/main/java/biz/zenpets/kennels/utils/adapters/reviews/ReviewsAdapter.java
+++ Kennels/src/main/java/biz/zenpets/kennels/utils/adapters/reviews/ReviewsAdapter.java
@@ -1,16 +1,26 @@
 package biz.zenpets.kennels.utils.adapters.reviews;
 
 import android.app.Activity;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RatingBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.afollestad.materialdialogs.DialogAction;
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.afollestad.materialdialogs.Theme;
+import com.crashlytics.android.Crashlytics;
 import com.facebook.drawee.view.SimpleDraweeView;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.facebook.imagepipeline.request.ImageRequestBuilder;
@@ -22,7 +32,12 @@ import java.util.Date;
 import java.util.Locale;
 
 import biz.zenpets.kennels.R;
+import biz.zenpets.kennels.utils.models.helpers.ZenApiClient;
 import biz.zenpets.kennels.utils.models.reviews.Review;
+import biz.zenpets.kennels.utils.models.reviews.ReviewsAPI;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewsVH> {
 
@@ -48,7 +63,7 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewsV
 
     @Override
     public void onBindViewHolder(@NonNull final ReviewsVH holder, final int position) {
-        Review data = arrReviews.get(position);
+        final Review data = arrReviews.get(position);
 
         /* SET THE USER'S DISPLAY PROFILE */
         String userDisplayProfile = data.getUserDisplayProfile();
@@ -96,64 +111,90 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewsV
             holder.txtKennelExperience.setText(data.getKennelExperience());
         }
 
-//        /* CHECK IF A REPLY HAS BEEN POSTED */
-//        String kennelReplyStatus = data.getKennelReplyStatus();
-//        if (kennelReplyStatus != null
-//                && !kennelReplyStatus.equalsIgnoreCase("")
-//                && !kennelReplyStatus.equalsIgnoreCase("null"))    {
-//            if (kennelReplyStatus.equalsIgnoreCase("0"))    {
-//                holder.txtReplyStatus.setVisibility(View.GONE);
-//                holder.txtReply.setVisibility(View.GONE);
-//            } else if (kennelReplyStatus.equalsIgnoreCase("1")) {
-//                String kennelOwnerName = data.getKennelOwnerName();
-//                String replyText = data.getKennelReplyText();
-//                Date dateReply = new Date(Long.parseLong(data.getKennelReplyTimestamp()) * 1000L);
-//                SimpleDateFormat sdfReply = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
-//                String replyTimestamp = sdfReply.format(dateReply);
-//                String strReplyFrom = activity.getString(R.string.review_reply_from_title) + " " + kennelOwnerName;
-//                SpannableStringBuilder builder = new SpannableStringBuilder(strReplyFrom);
-//                builder.setSpan(new StyleSpan(Typeface.BOLD), 0, strReplyFrom.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-//                String strTimestamp = " on " + replyTimestamp;
-//                builder.append(strTimestamp);
-//                holder.txtReplyStatus.setText(builder);
-//                holder.txtReply.setText(replyText);
-//                holder.txtReplyStatus.setVisibility(View.VISIBLE);
-//                holder.txtReply.setVisibility(View.VISIBLE);
-//            }
-//        } else {
-//            holder.txtReplyStatus.setVisibility(View.GONE);
-//            holder.txtReply.setVisibility(View.GONE);
-//        }
+        /* CHECK IF A REPLY HAS BEEN POSTED */
+        String kennelReplyStatus = data.getKennelReplyStatus();
+        if (kennelReplyStatus != null
+                && !kennelReplyStatus.equalsIgnoreCase("")
+                && !kennelReplyStatus.equalsIgnoreCase("null"))    {
+            if (kennelReplyStatus.equalsIgnoreCase("0"))    {
+                holder.txtReplyStatus.setVisibility(View.GONE);
+                holder.txtReply.setVisibility(View.GONE);
+                holder.imgvwEditReply.setVisibility(View.GONE);
+            } else if (kennelReplyStatus.equalsIgnoreCase("1")) {
+                String replyText = data.getKennelReplyText();
+                Date dateReply = new Date(Long.parseLong(data.getKennelReplyTimestamp()) * 1000L);
+                SimpleDateFormat sdfReply = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+                String replyTimestamp = sdfReply.format(dateReply);
+                String strReplyFrom = activity.getString(R.string.review_reply_from_title) + " ";
+                SpannableStringBuilder builder = new SpannableStringBuilder(strReplyFrom);
+                builder.setSpan(new StyleSpan(Typeface.BOLD), 0, strReplyFrom.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                builder.append(replyTimestamp);
+                holder.txtReplyStatus.setText(builder);
+                holder.txtReply.setText(replyText);
+                holder.txtReplyStatus.setVisibility(View.VISIBLE);
+                holder.txtReply.setVisibility(View.VISIBLE);
+                holder.imgvwEditReply.setVisibility(View.VISIBLE);
+            }
+        } else {
+            holder.txtReplyStatus.setVisibility(View.GONE);
+            holder.txtReply.setVisibility(View.GONE);
+            holder.imgvwEditReply.setVisibility(View.GONE);
+        }
+        
+        /* EDIT THE REPLY */
+        holder.imgvwEditReply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateReply(data.getKennelReviewID(), data.getKennelReplyText(), position);
+            }
+        });
+    }
 
-//        /* SHOW THE KENNEL OPTIONS POPUP MENU */
-//        holder.imgvwReviewOptions.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                PopupMenu pm = new PopupMenu(activity, holder.imgvwReviewOptions);
-//                pm.getMenuInflater().inflate(R.menu.pm_review_options, pm.getMenu());
-//                pm.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-//                    @Override
-//                    public boolean onMenuItemClick(MenuItem item) {
-//                        switch (item.getItemId())   {
-//                            case R.id.menuUnhelpful:
-//                                break;
-//                            case R.id.menuInappropriate:
-//                                break;
-//                            case R.id.menuSpam:
-//                                break;
-//                            case R.id.menuAbusive:
-//                                break;
-//                            case R.id.menuViolent:
-//                                break;
-//                            default:
-//                                break;
-//                        }
-//                        return false;
-//                    }
-//                });
-//                pm.show();
-//            }
-//        });
+    /** UPDATE THE KENNEL OWNER'S REPLY **/
+    private void updateReply(final String kennelReviewID, String kennelReplyText, final int position) {
+        final String timeStamp = String.valueOf(System.currentTimeMillis() / 1000);
+        new MaterialDialog.Builder(activity)
+                .title("Update your reply")
+                .content(null)
+                .inputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_CAP_SENTENCES | InputType.TYPE_TEXT_FLAG_MULTI_LINE)
+                .inputRange(5, 200)
+                .theme(Theme.LIGHT)
+                .typeface("Roboto-Medium.ttf", "Roboto-Regular.ttf")
+                .positiveText("Update")
+                .negativeText("Cancel")
+                .input("Update your reply...", kennelReplyText, false, new MaterialDialog.InputCallback() {
+                    @Override
+                    public void onInput(@NonNull final MaterialDialog dialog, CharSequence input) {
+                        ReviewsAPI apiInterface = ZenApiClient.getClient().create(ReviewsAPI.class);
+                        Call<Review> call = apiInterface.updateKennelReviewReply(kennelReviewID, input.toString(), timeStamp);
+                        call.enqueue(new Callback<Review>() {
+                            @Override
+                            public void onResponse(Call<Review> call, Response<Review> response) {
+                                if (response.isSuccessful())    {
+                                    dialog.dismiss();
+                                    Toast.makeText(activity, "Successfully updated the reply...", Toast.LENGTH_SHORT).show();
+                                    notifyItemChanged(position);
+                                } else {
+                                    dialog.dismiss();
+                                    Toast.makeText(activity, "Update failed...", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<Review> call, Throwable t) {
+//                                Log.e("FAILURE", t.getMessage());
+                                Crashlytics.logException(t);
+                            }
+                        });
+                    }
+                })
+                .onNegative(new MaterialDialog.SingleButtonCallback() {
+                    @Override
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 
     @NonNull
@@ -175,6 +216,9 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewsV
         TextView txtTimestamp;
         IconicsImageView imgvwLikeStatus;
         TextView txtKennelExperience;
+        TextView txtReplyStatus;
+        TextView txtReply;
+        IconicsImageView imgvwEditReply;
 
         ReviewsVH(View v) {
             super(v);
@@ -185,9 +229,9 @@ public class ReviewsAdapter extends RecyclerView.Adapter<ReviewsAdapter.ReviewsV
             txtTimestamp = v.findViewById(R.id.txtTimestamp);
             imgvwLikeStatus = v.findViewById(R.id.imgvwLikeStatus);
             txtKennelExperience = v.findViewById(R.id.txtKennelExperience);
-//            txtReplyStatus = v.findViewById(R.id.txtReplyStatus);
-//            txtReply = v.findViewById(R.id.txtReply);
-//            imgvwReviewOptions = v.findViewById(R.id.imgvwReviewOptions);
+            txtReplyStatus = v.findViewById(R.id.txtReplyStatus);
+            txtReply = v.findViewById(R.id.txtReply);
+            imgvwEditReply = v.findViewById(R.id.imgvwEditReply);
         }
     }
 }
--- Kennels/src/main/java/biz/zenpets/kennels/utils/models/reviews/ReviewsAPI.java
+++ Kennels/src/main/java/biz/zenpets/kennels/utils/models/reviews/ReviewsAPI.java
@@ -1,7 +1,10 @@
 package biz.zenpets.kennels.utils.models.reviews;
 
 import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface ReviewsAPI {
@@ -9,4 +12,12 @@ public interface ReviewsAPI {
     /** FETCH ALL REVIEWS OF A KENNEL **/
     @GET("fetchKennelReviews")
     Call<Reviews> fetchKennelReviews(@Query("kennelID") String kennelID);
+
+    /** UPDATE A KENNEL OWNER'S REPLY **/
+    @POST("updateKennelReviewReply")
+    @FormUrlEncoded
+    Call<Review> updateKennelReviewReply(
+            @Field("kennelReviewID") String kennelReviewID,
+            @Field("kennelReplyText") String kennelReplyText,
+            @Field("kennelReplyTimestamp") String kennelReplyTimestamp);
 }
